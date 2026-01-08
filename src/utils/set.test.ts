e7c4a12796ea1dad81d7572f9e90d99f5e932789
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2026-01-08 09:40:03 +0100

    add unit tests for `utils/set`

---
+++ src/utils/set.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from "vitest";
+import { excludeFromSet } from "./set";
+
+describe("excludeFromSet", () => {
+  it("value is not anymore in set when excluded", () => {
+    const oldSet = new Set(["a", "b", "c", "d"]);
+    const newSet = excludeFromSet(oldSet, ["b", "c"]);
+
+    expect(oldSet).toEqual(new Set(["a", "b", "c", "d"]));
+    expect(newSet).toEqual(new Set(["a", "d"]));
+  })
+
+  it("when value was not in set is still not in set after excluding", () => {
+    const oldSet = new Set(["a", "b", "c", "d"]);
+    const newSet = excludeFromSet(oldSet, ["e", "f"]);
+
+    expect(oldSet).toEqual(new Set(["a", "b", "c", "d"]));
+    expect(newSet).toEqual(oldSet);
+  })
+})
