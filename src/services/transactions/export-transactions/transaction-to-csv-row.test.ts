2c1e4319602f87b53fdc1e37bfac7f2dfe8392b5
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2026-01-29 13:44:51 +0100

    write unit tests for `streamTransactions` and `transactionToCsvRow`

---
+++ src/services/transactions/export-transactions/transaction-to-csv-row.test.ts
@@ -0,0 +1,21 @@
+import { describe, it, expect } from "vitest";
+import { transactionToCsvRow } from "./transaction-to-csv-row";
+import { getStandardTransactionResultJSON } from "@/test-utils/mocks/transactions";
+
+
+describe("transactionToCsvRow", () => {
+  const { ownerId, date, id, ...transaction } = getStandardTransactionResultJSON("1", 1, "1");
+  const iTransaction = { ...transaction, _id: id, date: new Date(date) };
+
+  it("transaction to csv row", () => {
+    const result = transactionToCsvRow(iTransaction as any);
+
+    expect(result).toEqual({
+      ...transaction,
+      date: date.slice(0, 10),
+      currencies: undefined,
+      exchangeRate: undefined,
+      sourceRefIndex: undefined,
+    });
+  })
+})
