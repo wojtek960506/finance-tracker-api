fbdc5f110aae9083acf1d2d54e0dbf6e219d1db9
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2026-02-03 14:17:36 +0100

    add service for creating category

--- src/routes/category-routes/handlers/create-category-handler.ts
+++ src/routes/category-routes/handlers/create-category-handler.ts
@@ -1,10 +1,7 @@
 import { CategoryDTO } from "@schemas/category";
-import { normalizeWhitespace } from "@utils/strings";
-import { CategoryType } from "@models/category-model";
+import { createCategory } from "@services/categories";
 import { FastifyReply, FastifyRequest } from "fastify";
-import { CategoryAlreadyExistsError } from "@utils/errors";
 import { AuthenticatedRequest } from "@routes/routes-types";
-import { findCategoryByName, persistCategory } from "@db/categories";
 
 
 export const createCategoryHandler = async (
@@ -12,18 +9,6 @@ export const createCategoryHandler = async (
   res: FastifyReply,
 ) => {
   const ownerId = (req as AuthenticatedRequest).userId;
-  const { name } = req.body;
-
-  const category = await findCategoryByName(name); 
-  if (category) throw new CategoryAlreadyExistsError(category.nameNormalized);
-
-  const props = {
-    ownerId,
-    type: "user" as CategoryType,
-    name: normalizeWhitespace(name),
-    nameNormalized: normalizeWhitespace(name).toLowerCase(),
-  }
-
-  const result = await persistCategory(props);
+  const result = await createCategory(ownerId, req.body);
   return res.code(201).send(result);
 }
