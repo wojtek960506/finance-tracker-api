88732097302219fe07f00a64bdd302d5aa92feda
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2026-02-06 20:33:26 +0100

    fix unit tests for preparing transaction props

--- src/services/transactions/prepare-exchange-props/prepare-exchange-props.test.ts
+++ src/services/transactions/prepare-exchange-props/prepare-exchange-props.test.ts
@@ -1,52 +1,47 @@
 import { describe, expect, it } from "vitest";
-import { randomObjectIdString } from "@utils/random";
+import { USER_ID_STR } from "@/test-utils/factories/general";
 import { prepareExchangeProps } from "./prepare-exchange-props";
+import { EXCHANGE_CATEGORY_ID_STR } from "@/test-utils/factories/category";
 import {
-  OLD_getTransactionExchangeDTO,
-  OLD_getExchangeTransactionProps,
-} from "@/test-utils/mocks/transactions";
+  getExchangeTransactionDTO,
+  getExchangeTransactionProps,
+  EXCHANGE_TXN_INCOME_SRC_IDX,
+  EXCHANGE_TXN_EXPENSE_SRC_IDX,
+} from "@/test-utils/factories/transaction";
 
 
 describe("prepareExchangeProps", () => {
   it("prepare props for create", () => {
-    const dto = OLD_getTransactionExchangeDTO();
-    const OWNER_ID = randomObjectIdString();
-    const EXPENSE_IDX = 1;
-    const INCOME_IDX = 2;
+    const dto = getExchangeTransactionDTO();
+    const mockProps = getExchangeTransactionProps(true);
 
     const {
       expenseTransactionProps,
       incomeTransactionProps,
     } = prepareExchangeProps(
       dto,
+      { categoryId: EXCHANGE_CATEGORY_ID_STR },
       {
-        ownerId: OWNER_ID,
-        sourceIndexExpense: EXPENSE_IDX,
-        sourceIndexIncome: INCOME_IDX
+        ownerId: USER_ID_STR,
+        sourceIndexExpense: EXCHANGE_TXN_EXPENSE_SRC_IDX,
+        sourceIndexIncome: EXCHANGE_TXN_INCOME_SRC_IDX,
       }
     );
 
-    const mockProps = OLD_getExchangeTransactionProps({
-      ownerId: OWNER_ID,
-      sourceIndexExpense: EXPENSE_IDX,
-      sourceIndexIncome: INCOME_IDX,
-    });
-
     expect(expenseTransactionProps).toEqual(mockProps.expenseProps);
     expect(incomeTransactionProps).toEqual(mockProps.incomeProps);
-  })
+  });
 
   it("prepare props for update", () => {
-    const dto = OLD_getTransactionExchangeDTO();
+    const dto = getExchangeTransactionDTO();
+    const mockProps = getExchangeTransactionProps();
 
     const {
       expenseTransactionProps,
       incomeTransactionProps,
-    } = prepareExchangeProps(dto);
-
-    const mockProps = OLD_getExchangeTransactionProps();
+    } = prepareExchangeProps(dto, { categoryId: EXCHANGE_CATEGORY_ID_STR });
 
     expect(expenseTransactionProps).toEqual(mockProps.expenseProps);
     expect(incomeTransactionProps).toEqual(mockProps.incomeProps);
-  })
-})
+  });
+});
