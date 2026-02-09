1da47c82eea19fedc748c8fb852d439b933fa7fe
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2026-02-09 19:25:36 +0100

    refactor `persist-transaction-pair` to use `withSession` wrapper

--- src/db/transactions/persist-transaction/persist-transaction-pair.ts
+++ src/db/transactions/persist-transaction/persist-transaction-pair.ts
@@ -1,51 +1,62 @@
-import { startSession } from "mongoose";
+import { ClientSession } from "mongoose";
+import { AppError } from "@utils/errors";
+import { withSession } from "@utils/with-session";
 import { serializeTransaction } from "@schemas/serializers";
 import { TransactionModel } from "@models/transaction-model";
 import { TransactionResponseDTO } from "@schemas/transaction";
 import { TransactionExchangeCreateProps, TransactionTransferCreateProps } from "./types";
 
 
-export async function persistTransactionPair<
+const persistTransactionPairHandler = async <
   T extends TransactionExchangeCreateProps | TransactionTransferCreateProps
 >(
+  session: ClientSession,
   expenseTransactionProps: T,
   incomeTransactionProps: T,
-): Promise<[TransactionResponseDTO, TransactionResponseDTO]> {
-  let expenseTransaction;
-  let incomeTransaction;
-  const session = await startSession();
+): Promise<[TransactionResponseDTO, TransactionResponseDTO]> => {
+  const [
+    { _id: expenseTransactionId },
+    { _id: incomeTransactionId },
+  ] = await TransactionModel.create(
+    [expenseTransactionProps, incomeTransactionProps],
+    { session, ordered: true }
+  );
   
-  try {
-    await session.withTransaction(async () => {
-      const [
-        { _id: expenseTransactionId },
-        { _id: incomeTransactionId },
-      ] = await TransactionModel.create(
-        [expenseTransactionProps, incomeTransactionProps],
-        { session, ordered: true }
-      );
-      
-      expenseTransaction = await TransactionModel.findOneAndUpdate(
-        { _id: expenseTransactionId },
-        { refId: incomeTransactionId },
-        { session, new: true }
-      ).populate([
-        { path: 'categoryId', select: '_id type name' },
-      ]);
-      incomeTransaction = await TransactionModel.findOneAndUpdate(
-        { _id: incomeTransactionId },
-        { refId: expenseTransactionId },
-        { session, new: true }
-      ).populate([
-        { path: 'categoryId', select: '_id type name' },
-      ]);
-    })
-  } finally {
-    await session.endSession();
-  }
+  const expenseTransaction = await TransactionModel.findOneAndUpdate(
+    { _id: expenseTransactionId },
+    { refId: incomeTransactionId },
+    { session, new: true }
+  ).populate([
+    { path: 'categoryId', select: '_id type name' },
+  ]);
+  const incomeTransaction = await TransactionModel.findOneAndUpdate(
+    { _id: incomeTransactionId },
+    { refId: expenseTransactionId },
+    { session, new: true }
+  ).populate([
+    { path: 'categoryId', select: '_id type name' },
+  ]);
 
   return [
     serializeTransaction(expenseTransaction!),
     serializeTransaction(incomeTransaction!),
   ]
+}
+
+export const persistTransactionPair = async <
+  T extends TransactionExchangeCreateProps | TransactionTransferCreateProps
+>(
+  expenseTransactionProps: T,
+  incomeTransactionProps: T,
+): Promise<[TransactionResponseDTO, TransactionResponseDTO]> => {
+
+  const result = await withSession(
+    persistTransactionPairHandler,
+    expenseTransactionProps,
+    incomeTransactionProps,
+  );
+  if (!result)
+    throw new AppError(500, "Error during executing operation within MongoDB session");
+
+  return result;
 }
