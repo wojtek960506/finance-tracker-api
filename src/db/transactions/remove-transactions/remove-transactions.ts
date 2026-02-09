5333aabefa2133446d575de6eab09033ee2c7b1c
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2026-02-09 22:52:20 +0100

    apply small changes to fix failed deploy

--- src/db/transactions/remove-transactions/remove-transactions.ts
+++ src/db/transactions/remove-transactions/remove-transactions.ts
@@ -1,9 +1,10 @@
+import { DeleteManyReply } from "@routes/routes-types";
 import { TransactionModel } from "@models/transaction-model";
 
 
 type RemoveQuery = { ownerId?: string }
 
-export const removeTransactions = async (ownerId?: string) => {
+export const removeTransactions = async (ownerId?: string): Promise<DeleteManyReply> => {
   const query: RemoveQuery = {};
   if (ownerId !== undefined) query.ownerId = ownerId;
   return TransactionModel.deleteMany(query);
