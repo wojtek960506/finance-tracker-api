7248dcebdfc619052c9d47b86be46b858ba92251
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2026-03-06 15:33:05 +0100

    add ESLint + Preetier rules

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import importPlugin from 'eslint-plugin-import'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+
+export default tseslint.config(
+  {
+    ignores: ['dist/**', 'coverage/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      import: importPlugin,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'sort-imports': 'off',
+      'import/order': 'off',
+      'import/no-duplicates': 'error',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^node:'],
+            ['^\\u0000'],
+            ['^@?\\w'],
+            ['^(@app|@auth|@user|@category|@transaction|@payment-method|@shared|@plugins|@utils|@/)(/.*|$)'],
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  {
+    rules: {
+      'no-undef': 'off',
+    },
+  },
+)
