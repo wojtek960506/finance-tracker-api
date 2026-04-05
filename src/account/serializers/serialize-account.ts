71b46fc22685d2af9fb3158d46cbde27f0a2aee5
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2026-04-05 21:58:14 +0200

    refactor: remove unused named resource serializers
    - delete obsolete account, category, and payment method serializer modules
    - remove serializer index exports that are no longer referenced
    - drop serializer tests for the removed named resource serializers

--- src/account/serializers/serialize-account.ts
+++
@@ -1,7 +0,0 @@
-import { IAccount } from '@account/model';
-import { AccountResponseDTO } from '@account/schema';
-import { serializeNamedResource } from '@shared/named-resource';
-
-export const serializeAccount = (account: IAccount): AccountResponseDTO => {
-  return serializeNamedResource<AccountResponseDTO>(account);
-};
