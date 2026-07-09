aa2875c10a9f91589a9ea82061a76370813a587d
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2026-07-10 00:50:18 +0200

    add potential TODO for findTransactionResourceIds

--- src/transaction/db/find-transaction-resource-ids/find-transaction-resource-ids.ts
+++ src/transaction/db/find-transaction-resource-ids/find-transaction-resource-ids.ts
@@ -3,6 +3,8 @@ import { FilterQuery } from 'mongoose';
 import { ITransaction, TransactionModel } from '@transaction/model';
 
 export const findTransactionResourceIds = async (filter: FilterQuery<ITransaction>) => {
+  // TODO: Consider replacing these 3 distinct queries with a single aggregation
+  // if filtered exports become a performance hotspot.
   const [accountIds, categoryIds, paymentMethodIds] = await Promise.all([
     TransactionModel.distinct('accountId', filter),
     TransactionModel.distinct('categoryId', filter),
